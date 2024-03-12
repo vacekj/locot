a49da42ed4dae14640085e5fc2e1eca0782633de
Atris <vacekj@outlook.com>
2024-03-12 16:16:52 +0100

    feat: dates, sorting

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 use anyhow::Error;
-use chrono::{Date, DateTime, Local, TimeZone};
-use git2::Repository;
+use chrono::{Date, DateTime, Local, TimeZone, Utc};
+use git2::{Repository, Time};
+use itertools::Itertools;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::os::macos::raw::stat;
 use std::{env, io};
 use tempfile::tempdir;
-use tokei::{Config, Languages};
+use tokei::{Config, LanguageType, Languages};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Stat {
     commit: String,
+    date: DateTime<Utc>,
     total: usize,
     langs: Vec<usize>,
 }
@@ -29,13 +32,7 @@ fn main() -> Result<(), Error> {
     let mut revwalk = repo.revwalk().expect("Creating revwalk not to fail");
     revwalk.push_head()?;
 
-    let mut stats: HashMap<String, Languages> = HashMap::new();
-
-    /*TODO:
-    1. get the element with the most languages (track all languages from the beginning)
-    2. write the languages as headers
-     3. transform the languages in the btreemap to a hashmap, fill in the missing ones as 0, sort by name, and then convert to vec of tuples
-      4. serialize into the Stat struct, keeping the sorting */
+    let mut stats: HashMap<(String, DateTime<Utc>), Languages> = HashMap::new();
 
     for oid in revwalk.flatten() {
         let commit_hash = oid.clone().to_string();
@@ -59,20 +56,67 @@ fn main() -> Result<(), Error> {
         langs.get_statistics(&[&tmp_dir], &[".git", "target"], &Config::default());
         let time = DateTime::from_timestamp(commit.time().seconds(), 0)
             .expect("Creating time from commit not to fail");
-        stats.insert(commit_hash, langs);
+        stats.insert((commit_hash, time), langs);
     }
 
     let mut wtr = csv::WriterBuilder::new()
         .has_headers(false)
         .from_writer(io::stdout());
 
-    for (commit, stat) in stats {
+    let mut all_langs = stats
+        .values()
+        .collect::<Vec<&Languages>>()
+        .iter_mut()
+        .flat_map(|lang| {
+            return lang.iter().filter_map(|(langtype, lang)| {
+                let mut lang = lang.clone();
+                lang.total();
+
+                if lang.lines() > 0 {
+                    Some(*langtype)
+                } else {
+                    None
+                }
+            });
+        })
+        .unique()
+        .collect::<Vec<LanguageType>>();
+
+    #[derive(Serialize)]
+    struct Header {
+        commit: String,
+        time: String,
+        total: String,
+        langs: Vec<String>,
+    }
+
+    wtr.serialize(Header {
+        commit: "Commit".into(),
+        time: "Time".into(),
+        total: "Total (code)".into(),
+        langs: all_langs.iter().map(|t| t.to_string()).collect(),
+    })
+    .expect("Writing header not to fail");
+
+    for (commit, stat) in stats.iter().sorted_by_key(|(commit, _)| &commit.1) {
         wtr.serialize(Stat {
-            commit,
+            commit: commit.0.clone(),
+            date: commit.1,
             total: stat.total().code,
-            langs: stat
-                .iter()
-                .map(|(typ, lang)| lang.reports.last().unwrap().stats.code)
+            langs: all_langs
+                .iter_mut()
+                .map(|language_type| {
+                    let found_lang = stat
+                        .iter()
+                        .find(|(typ, lang)| typ.eq(&language_type))
+                        .map(|(typ, lang)| lang.clone());
+                    if let Some(mut lang) = found_lang {
+                        lang.total();
+                        lang.lines()
+                    } else {
+                        0
+                    }
+                })
                 .collect(),
         })?;
     }
